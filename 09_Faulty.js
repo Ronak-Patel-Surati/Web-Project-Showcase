ef3ad6f54d7546155eb0f10876b4e6a345694931
Ronak Patel <ronitsinghronak@proton.me>
2024-08-30 21:42:08 +0530

    Update 09_Faulty.js

--- 09_Faulty.js
+++ 09_Faulty.js
@@ -1,7 +1,7 @@
 let random = math.random()
 let a = prompt("enter first number")
-let b = prompt("enter second number")
 let c = prompt("enter operation")
+let b = prompt("enter second number")
 
 let obj = {
     "+":"-",
@@ -14,5 +14,15 @@ c  = obj[c]
 if (random > 0.1) {
     alert(`The Result is ${eval(`${a} ${c} ${b}`)}`)
 }else{
-    
+    c =  obj[c]
+    alert(`The Result is ${eval(`${a} ${c} ${b}`)}`)
 }
+prompt  
+for (const key in object) {
+    if (Object.prototype.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
+
+prompt('');
